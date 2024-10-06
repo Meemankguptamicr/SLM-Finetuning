106a74e047d23b5d8bbc442acf4311592acac216
Liana <liana.napalkova@gmail.com>
2024-10-06 17:50:58 +0200

    Update create_environment.sh

--- infrastructure/azure_ml/create_environment.sh
+++ infrastructure/azure_ml/create_environment.sh
@@ -3,7 +3,7 @@
 set -e
 
 ENV_NAME=${ENV_NAME:-"gpu-environment"}
-FILE=${FILE:-"./infrastructure/azure_ml/gpu_docker/gpu_environment.yml"}
+FILE=${FILE:-"./infrastructure/azure_ml/gpu_docker/"}
 RESOURCE_GROUP=${RESOURCE_GROUP:-"my-resource-group"}
 WORKSPACE_NAME=${WORKSPACE_NAME:-"my-workspace"}
 
