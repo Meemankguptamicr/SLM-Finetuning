b9b7b328b53672dd9053c567543e6cbc0de81c95
Liana <liana.napalkova@gmail.com>
2024-10-06 17:47:13 +0200

    Update create_environment.sh

--- infrastructure/azure_ml/create_environment.sh
+++ infrastructure/azure_ml/create_environment.sh
@@ -21,7 +21,7 @@ echo "Registering environment '$ENV_NAME' in Azure ML..."
 
 if [ -f "$FILE" ]; then
     echo "Using Dockerfile for environment creation..."
-    az ml environment create" \
+    az ml environment create \
         --name "$ENV_NAME" \
         --file "$FILE" \
         --resource-group "$RESOURCE_GROUP" \
