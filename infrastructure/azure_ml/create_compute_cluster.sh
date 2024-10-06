19de073c5edaf15dde45a04619be928b0bb8dedb
Liana <liana.napalkova@gmail.com>
2024-10-06 18:15:48 +0200

    Update create_compute_cluster.sh

--- infrastructure/azure_ml/create_compute_cluster.sh
+++ infrastructure/azure_ml/create_compute_cluster.sh
@@ -4,6 +4,7 @@ set -e
 
 COMPUTE_NAME=${COMPUTE_NAME:-"gpu-cluster"}
 VM_SIZE=${VM_SIZE:-"Standard_NC24ads_A100_v4"}
+COMPUTE_TYPE=${COMPUTE_TYPE:-"AmlCompute"}
 MIN_NODES=${MIN_NODES:-0}
 MAX_NODES=${MAX_NODES:-1}
 RESOURCE_GROUP=${RESOURCE_GROUP:-"my-resource-group"}
@@ -16,8 +17,10 @@ fi
 
 echo "Creating Azure ML Compute Clusters..."
 
-az ml compute create --name "$COMPUTE_NAME" \
+az ml compute create \
+    --name "$COMPUTE_NAME" \
     --size "$VM_SIZE" \
+    --type "$COMPUTE_TYPE" \
     --min-instances "$MIN_NODES" \
     --max-instances "$MAX_NODES" \
     --resource-group "$RESOURCE_GROUP" \
