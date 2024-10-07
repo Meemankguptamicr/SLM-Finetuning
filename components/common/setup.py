5843a3699d0b7ae6bd71189fd89c78120e741669
Liana <liana.napalkova@gmail.com>
2024-10-07 11:08:21 +0200

    Update setup.py

--- components/common/setup.py
+++ components/common/setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='common_utils',
     version='0.1.0',
+    author='SFTA team',
     description='Package for the SLM project utilities',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
